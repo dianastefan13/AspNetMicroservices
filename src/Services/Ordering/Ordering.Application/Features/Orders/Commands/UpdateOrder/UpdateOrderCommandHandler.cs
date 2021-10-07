051cac5b1ef9a59dfcdf10b01a2266508e16ed1d
diana <stefan.diana.m@gmail.com>
2021-10-07 14:48:07 +0300

    Implemented update order command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> logger;
+        public UpdateOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<UpdateOrderCommandHandler> logger)
+        {
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+            this.logger = logger;
+        }
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await orderRepository.GetByIdAsync(request.Id);
+
+            if (orderToUpdate == null)
+            {
+                logger.LogError("Order does not exist in DB");
+            }
+
+            mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await orderRepository.UpdateAsync(orderToUpdate);
+
+            logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+
+            return Unit.Value;
+        }
+    }
+}
