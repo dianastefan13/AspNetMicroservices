069e2e88d72f29a83c5b422493c016ebfaf36b58
diana <stefan.diana.m@gmail.com>
2021-10-16 15:43:17 +0300

    Implement subscribe for BasketCheckoutEvent in Ordering.API microservice

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        private readonly IMapper mapper;
+        private readonly IMediator mediator;
+        private readonly ILogger<BasketCheckoutConsumer> logger;
+
+        public BasketCheckoutConsumer(IMapper mapper, IMediator mediator, ILogger<BasketCheckoutConsumer> logger)
+        {
+            this.mapper = mapper;
+            this.mediator = mediator;
+            this.logger = logger;
+        }
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            var command = mapper.Map<CheckoutOrderCommand>(context.Message);
+
+            var result = await mediator.Send(command);
+
+            logger.LogInformation("BasketCheckoutEvent consumed successfully. Created Order Id : {newOrderId}", result);
+        }
+    }
+}
